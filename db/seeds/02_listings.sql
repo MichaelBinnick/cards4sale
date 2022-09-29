6261fff8c707edda1188ca2e8165fa5d7a04d376
Tommy Tran <haitran1995@gmail.com>
2022-09-29 17:31:15 -0400

    pre merge

--- db/seeds/02_listings.sql
+++ db/seeds/02_listings.sql
@@ -5,3 +5,4 @@ INSERT INTO listings (title, description, price, owner_id, time_created)
 VALUES ('Nicks Car', 'Its a sweet speedster!', 10000, 2, '2022-09-18 12:01:00');
 INSERT INTO listings (title, description, price, owner_id, time_created)
 VALUES ('Mikes Trike', 'Its a kind of lame kid bike...training wheels? Reall?', 50, 3, '2022-09-19 17:27:00');
+
--- server.js
+++ server.js
@@ -83,14 +83,11 @@ app.get('/', (req, res) => {
   if (userID) {
     return databaseFn.getUserWithId(userID)
     .then(dbUser => {
-      console.log(`returned user from Id:`, dbUser);
       templateVars.user = dbUser;
-      console.log('logged in successfully as: ', dbUser.name)
 
       databaseFn.getLimitListings(8)
         .then(listings => {
           templateVars.listings = listings;
-          console.log(`listings from fn:`, listings);
           return res.render('index', templateVars);
         })
       // route logic here
@@ -237,7 +234,7 @@ app.post('/listings', (req, res) => {
     .then(dbUser => {
       console.log(`returned user from Id:`, dbUser);
       console.log('logged in successfully as: ', dbUser.name)
-
+      console.log(`req.body is:`, req.body);
       // tommy's route logic
       const ownerId = dbUser.id;
 
@@ -274,9 +271,7 @@ app.get('/myListings', (req, res) => {
   if (userID) {
     return databaseFn.getUserWithId(userID)
     .then(dbUser => {
-      console.log(`returned user from Id:`, dbUser);
       templateVars.user = dbUser;
-      console.log('logged in successfully as: ', dbUser.name)
 
       let filteredDatabase = {}
       for(let keys in listings) {
@@ -289,7 +284,6 @@ app.get('/myListings', (req, res) => {
       return databaseFn.getListingsByOwnerId(dbUser.id)
     })
     .then(listings => {
-      console.log(`return value of promise/result.rows:`, listings);
       // console.log('filtered db:', filteredDatabase)
       templateVars['listings'] = {};
       for (let listing of listings) {
@@ -302,11 +296,11 @@ app.get('/myListings', (req, res) => {
           owner_id: listing.owner_id,
           owner_name: listing.owner_name,
           is_removed: listing.is_removed,
-          id: listing.id
+          id: listing.id,
+          time_created: listing.time_created
         }
 
       }
-      console.log(`formatted return listings for ejs view:`, templateVars.listings);
       return res.render('myListings', templateVars);
     })
     .catch(e => {
@@ -347,6 +341,7 @@ app.get('/listings/:id', (req, res) => {
         templateVars.listing_id = listing.listing_id;
         templateVars.owner_name = listing.owner_name;
         templateVars.owner_id = listing.owner_id;
+        templateVars.time_created = listing.time_created;
       // end of route logic
 
       return res.render('single_listing', templateVars);
@@ -440,13 +435,10 @@ app.get('/messages', (req, res) => {
   if (userID) {
     return databaseFn.getUserWithId(userID)
     .then(dbUser => {
-      console.log(`returned user from Id:`, dbUser);
       templateVars.user = dbUser;
-      console.log('logged in successfully as: ', dbUser.name)
 
       databaseFn.getMessages(userID)
         .then(messages => {
-          console.log("messages from DB: ", messages);
           const messageThreads = getMessagesByThread(messages);
           templateVars.messages = messageThreads;
           return res.render('messages', templateVars);
@@ -548,7 +540,7 @@ app.post('/listings/:ownerID/:listingsID', (req, res) => {
   const message = {
     sender_id: userID,
     recipient_id: id,
-    contents: Object.keys(req.body)[0],
+    contents: req.body,
     listing_id: listingID
   }
 
@@ -569,21 +561,89 @@ app.post('/listings/:ownerID/:listingsID', (req, res) => {
 
 
 // Delete listing
-app.post("/listings/delete", (req, res) => {
+app.post("/delete", (req, res) => {
+
+  const {userID} = req.session;
+
   const listingId = req.body.listingKey;
 
   databaseFn.changeListingRemovalStatus(listingId, true)
   .then(result => {
     console.log(`listing has been successfully removed`);
+    document.reload();
   })
   .catch(e => {
-    console.log(e);
+    // console.log(e);
     res.send(e);
   })
+});
 
+// Mark listings as SOLD
+app.post("/mark_sold", (req, res) => {
+
+  const {userID} = req.session;
+
+  const listingId = req.body.listingKey;
+
+  databaseFn.changeListingSoldStatus(listingId, true)
+  .then(result => {
+    console.log(`listing has been successfully marked as sold`);
+    document.reload();
+  })
+  .catch(e => {
+    // console.log(e);
+    res.send(e);
+  })
+});
+
+
+app.post('/filtered', (req, res) => {
+  const minPrice = req.body.minPrice;
+  const maxPrice = req.body.maxPrice;
+
+  res.redirect(`/filtered/${minPrice}/${maxPrice}`)
+})
+
+app.get('/filtered/:min/:max', (req, res) => {
+  const {userID} = req.session;
+  const templateVars = {user: undefined};
+  if (userID) {
+    return databaseFn.getUserWithId(userID)
+    .then(dbUser => {
+      templateVars.user = dbUser;
+
+      console.log(`req.params are:`, req.params);
+      const filters = {minPrice: req.params.min, maxPrice: req.params.max};
+      console.log(`filters are:`, filters);
+
+      databaseFn.getFilteredListings(8, filters)
+        .then(listings => {
+          templateVars.listings = listings;
+          console.log(`filtered listings from fn:`, listings);
+          return res.render('index', templateVars);
+        })
+      // route logic here
+
+    })
+    .catch(e => {
+      console.log(e);
+      res.send(e);
+    })
   }
-);
+  /* end of required block */
 
+  // if user isn't logged in, do the same
+  // needs to be repeated because above logic only happens in async callback
+  databaseFn.getFilteredListings(8, filters)
+        .then(listings => {
+          templateVars.listings = listings;
+          return res.render('index', templateVars);
+        })
+        .catch(e => {
+          console.log(e);
+          res.send(e);
+        })
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
